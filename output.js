b97391cd17bc72f6048d721e173d1f1d9e73ff61
invalid-user <user@invaliduser.uk.to>
2020-09-28 12:48:58 +0100

    Create output.js

---
+++ output.js
@@ -0,0 +1,63 @@
+const jsonURL = 'https://olddigibox.github.io/sky-box-lookup/database.json'
+
+function getversion() {
+
+    //container for info
+    let infocontainer = document.getElementById('infocontainer')
+
+    //get value from um input
+    let textinput = document.getElementById('textinput').value
+
+    //make var name which is a div element
+    let name = document.createElement('div')
+    let link = document.createElement('div')
+    let confirmation = document.createElement('div')
+
+
+    fetch(jsonURL, { method: "Get" })
+        .then(res => res.json())
+        .then((json) => {
+
+            removeDivs()
+                //get name of Version Number
+            name.innerText = `Name: ${json["a" + textinput].name}`
+                //get link of Version Number
+            link.innerText = `Link: ${json["a" + textinput].name}`
+                //get confirmation of Version Number
+            confirmation.innerText = `confirmation: ${json["a" + textinput].name}`
+
+            addClass()
+            append()
+
+            //functions 
+            function removeDivs() {
+                //elements with class name infotext
+                var infotext = document.querySelector('.infotext');
+                //if element in infocontainer with class infotext remove
+                if (infocontainer.contains(infotext)) {
+                    infocontainer.querySelectorAll('*').forEach(n => n.remove())
+                }
+            }
+
+            function addClass() {
+                //add the class infotext to all divs
+                name.className += "infotext";
+                link.className += "infotext";
+                confirmation.className += "infotext";
+            }
+
+            function append() {
+                //add them to infocontainer div
+                infocontainer.append(name)
+                infocontainer.append(link)
+                infocontainer.append(confirmation)
+            }
+
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+
+
+}
